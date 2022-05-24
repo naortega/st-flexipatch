ee5000796cbba8c7227e68a083cdeb5bdc1d71a6
Nicolás Ortega Froysa <nicolas@ortegas.org>
2022-05-24 12:30:34 +0200

    Set terminus as font.

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "xos4 Terminus:style=Regular:pixelsize=18:antialias=true:autohint=true";
 #if FONT2_PATCH
 /* Spare fonts */
 static char *font2[] = {
